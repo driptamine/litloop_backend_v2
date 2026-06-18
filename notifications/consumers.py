113b554b9c7534636009a35d40357b1255fce314
Flipworld <talga.tawit@gmail.com>
2026-06-19 04:53:21 +0500

    Make consumer resilient to channel layer receive errors

--- notifications/consumers.py
+++ notifications/consumers.py
@@ -1,12 +1,56 @@
 import logging
+import functools
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from channels.db import database_sync_to_async
+from channels.exceptions import StopConsumer
+from channels.layers import get_channel_layer
+from channels.utils import await_many_dispatch
 from django.contrib.auth.models import AnonymousUser
 
 logger = logging.getLogger(__name__)
 
 
 class NotificationConsumer(AsyncJsonWebsocketConsumer):
+    async def __call__(self, scope, receive, send):
+        self.scope = scope
+        self.channel_layer = get_channel_layer(self.channel_layer_alias)
+        if self.channel_layer is not None:
+            try:
+                self.channel_name = await self.channel_layer.new_channel()
+            except Exception as e:
+                logger.error(f"Failed to create channel: {e}")
+                self.channel_layer = None
+        if self.channel_layer is not None:
+            self.channel_receive = functools.partial(
+                self.channel_layer.receive, self.channel_name
+            )
+        if self._sync:
+            self.base_send = async_to_sync(send)
+        else:
+            self.base_send = send
+        try:
+            if self.channel_layer is not None:
+                await await_many_dispatch(
+                    [receive, self._safe_channel_receive], self.dispatch
+                )
+            else:
+                await await_many_dispatch([receive], self.dispatch)
+        except StopConsumer:
+            pass
+
+    async def _safe_channel_receive(self):
+        try:
+            return await self.channel_receive()
+        except Exception as e:
+            logger.error(f"Channel layer receive error (swallowed): {e}")
+            return None
+
+    async def dispatch(self, message):
+        if message is None:
+            return
+        await super().dispatch(message)
+
     async def connect(self):
         self.user_id = None
         self.room_group_name = None
